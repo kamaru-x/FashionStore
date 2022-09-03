7752a9eb0555636f882cfefa134f03c50dcc49c0
kamaru-x <xin.kamaru@gmail.com>
2022-09-03 14:37:31 +0530

    first commit

--- FashionStore/settings.py
+++ FashionStore/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'userapp',
     'companyapp',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,4 +127,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
